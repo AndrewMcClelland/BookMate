f4089dbd0fd003381812294f950d9005fbb8f41c
Andrew McClelland <anmcclel@microsoft.com>
2021-05-08 20:54:49 -0400

    Updating TwilioHandler with latest code

--- TwilioHandler.py
+++ TwilioHandler.py
@@ -2,23 +2,27 @@ from datetime import datetime
 from twilio.rest import Client
 
 class TwilioHandler:
-    def __init__(self, accountSID, authToken, toNumber, fromNumber):
+    def __init__(self, accountSID, authToken, toNumbers, fromNumber):
         self.accountSID = accountSID
         self.authToken = authToken
-        self.toNumber = fromNumber
+        self.toNumbers = toNumbers
         self.fromNumber = fromNumber
     
-    def send_sms(self, body):
+    def sendSms(self, body, sendToMeOnly = False):
+
+        to_numbers = [self.toNumbers.split(',')[0]] if sendToMeOnly else self.toNumbers.split(',')
+
         client = Client(self.accountSID, self.authToken) 
         
         maxBodyChars = 1500
         messageBodies = [body[i: i + maxBodyChars] for i in range(0, len(body), maxBodyChars)]
 
-        for messageBody in messageBodies:
-            message = client.messages.create( 
-                                    from_=self.fromNumber,
-                                    body=messageBody,      
-                                    to=self.toNumber
-                                    )
-            
-        print("Sent SMS at " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + "\t Message:" + message.sid)
+        for to_number in self.toNumbers:
+            for messageBody in messageBodies:
+                message = client.messages.create( 
+                                        from_=self.fromNumber,
+                                        body=messageBody,      
+                                        to=to_number
+                                        )
+                
+                print("Sent SMS at {} - To: {}\tSID: {}".format(datetime.now().strftime("%H:%M:%S on %m/%d/%Y"), str(message.to), message.sid))
