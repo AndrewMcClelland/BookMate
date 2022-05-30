689f26cd9f2d6ef753968a41d85c20a8de8ee99d
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-05-30 10:09:11 -0700

    Add Startup class to Api

--- BookMate.Core.Api/Program.cs
+++ BookMate.Core.Api/Program.cs
@@ -1,46 +1,20 @@
+// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 namespace BookMate.Core.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc(
-                    name: "v1",
-                    info: new Microsoft.OpenApi.Models.OpenApiInfo
-                    {
-                        Title = "BookMate.Core.Api",
-                        Version = "v1",
-                    });
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint(
-                        url: "/swagger/v1/swagger.json",
-                        name: "BookMate.Core.Api");
-                });
-            }
-
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-            app.Run();
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
         }
     }
 }
