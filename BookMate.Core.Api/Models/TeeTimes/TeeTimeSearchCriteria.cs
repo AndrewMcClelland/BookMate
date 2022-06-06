19860042f5d2e1b223dfbe916fbced0a4d42d6ff
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-05 22:52:59 -0700

    FOUNDATIONS: Implement ForeUpSoftwareService RetrieveAllAvailableTeeTimesAsync method

---
+++ BookMate.Core.Api/Models/TeeTimes/TeeTimeSearchCriteria.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using System;
+
+namespace BookMate.Core.Api.Models.TeeTimes
+{
+    public class TeeTimeSearchCriteria
+    {
+        public string CourseId { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public TeeTimeHoles Holes { get; set; }
+        public TeeTimePlayers Players { get; set; }
+    }
+}
---
+++ BookMate.Core.Api/Services/Foundations/ForeUpSoftware/ForeUpSoftwareService.cs
@@ -0,0 +1,80 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookMate.Core.Api.Brokers.BookingSystems.ForeUpSoftwareBookingSystems;
+using BookMate.Core.Api.Brokers.Loggings;
+using BookMate.Core.Api.Models.Externals.ForeUpSoftware;
+using BookMate.Core.Api.Models.TeeTimes;
+using BookMate.Core.Api.Models.TeeTimes.ForeUpSoftware;
+
+namespace BookMate.Core.Api.Services.Foundations.ForeUpSoftware
+{
+    public partial class ForeUpSoftwareService : IForeUpSoftwareService
+    {
+        private readonly IForeUpSoftwareBookingSystemBroker foreUpSoftwareBookingSystemBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ForeUpSoftwareService(IForeUpSoftwareBookingSystemBroker foreUpSoftwareBookingSystemBroker, ILoggingBroker loggingBroker)
+        {
+            this.foreUpSoftwareBookingSystemBroker = foreUpSoftwareBookingSystemBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<List<TeeTime>> RetrieveAllAvailableTeeTimesAsync(ForeUpSoftwareTeeTimeSearchCriteria teeTimeSearchCriteria)
+        {
+            var externalForeUpSoftwareBookingCriteria = new ExternalForeUpSoftwareBookingCriteria
+            {
+                CourseId = teeTimeSearchCriteria.CourseId,
+                Date = teeTimeSearchCriteria.Date.Date.ToString("MM-dd-yyyy"),
+                Time = "all",
+                Holes = GetForeUpSoftwareHoles(teeTimeSearchCriteria),
+                Players = GetForeUpSoftwarePlayers(teeTimeSearchCriteria),
+                BookingClass = teeTimeSearchCriteria.BookingClass,
+                SpecialsOnly = "0",
+            };
+
+            List<ExternalForeUpSoftwareTeeTime> externalForeUpSoftwareTeeTimes = await this.foreUpSoftwareBookingSystemBroker.GetAvailableTeeTimes(externalForeUpSoftwareBookingCriteria);
+
+            return externalForeUpSoftwareTeeTimes.Select(AsTeeTime).ToList();
+        }
+
+        private static string GetForeUpSoftwareHoles(TeeTimeSearchCriteria teeTimeSearchCriteria)
+        {
+            return teeTimeSearchCriteria.Holes switch
+            {
+                TeeTimeHoles.Nine => "9",
+                TeeTimeHoles.Eighteen => "18",
+                _ => "all"
+            };
+        }
+
+        private static string GetForeUpSoftwarePlayers(TeeTimeSearchCriteria teeTimeSearchCriteria)
+        {
+            return teeTimeSearchCriteria.Players switch
+            {
+                TeeTimePlayers.One => "1",
+                TeeTimePlayers.Two => "2",
+                TeeTimePlayers.Three => "3",
+                TeeTimePlayers.Four => "4",
+                _ => "0"
+            };
+        }
+
+        private static readonly Func<ExternalForeUpSoftwareTeeTime, TeeTime> AsTeeTime = externalForeUpSoftwareTeeTime =>
+        {
+            return new TeeTime
+            {
+                CourseName = externalForeUpSoftwareTeeTime.CourseName,
+                DateTime = DateTimeOffset.Parse(externalForeUpSoftwareTeeTime.Time),
+                Holes = Int32.Parse(externalForeUpSoftwareTeeTime.Holes),
+                Players = externalForeUpSoftwareTeeTime.AvailableSpots,
+                Cost = externalForeUpSoftwareTeeTime.GreenFee,
+            };
+        };
+    }
+}
