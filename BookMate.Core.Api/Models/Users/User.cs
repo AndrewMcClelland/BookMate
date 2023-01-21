925f01fb6b0832172a30ac97f7c1a14e11ea6d83
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2023-01-20 22:33:01 -0500

    MODELS: Add User Models

---
+++ BookMate.Core.Api/Models/Users/User.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace BookMate.Core.Api.Models.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IDictionary<string, string> AuthTokens { get; set; }
+    }
+}
